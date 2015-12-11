9b3623247954072a525f9be4cd5cc6e65d16aeb6
iansrobinson <iansrobinson@gmail.com>
2015-12-11 12:05:19 +0000

    Ensure AsyncFileOpener throws exception if problem creating output stream.

--- mysql-connector/src/test/java/org/neo4j/mysql/AsyncFileOpenerTest.java
+++ mysql-connector/src/test/java/org/neo4j/mysql/AsyncFileOpenerTest.java
@@ -1,15 +1,27 @@
 package org.neo4j.mysql;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import org.neo4j.utils.ResourceRule;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+
+import static org.neo4j.utils.TemporaryDirectory.temporaryDirectory;
 
 public class AsyncFileOpenerTest
 {
+    @Rule
+    public final ResourceRule<File> tempDirectory = new ResourceRule<>( temporaryDirectory() );
+
     @Test
     public void shouldRethrowDependencyExceptions() throws Exception
     {
@@ -32,6 +44,22 @@ public class AsyncFileOpenerTest
         {
             // then
             assertEquals( e, expectedException );
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionIfCreatingOutputStreamFails() throws Exception
+    {
+        try
+        {
+            // when
+            new AsyncFileOpener( tempDirectory.get(), 1000, mock( Exceptions.class ) ).open();
+            fail( "Expected IOException" );
+        }
+        catch ( IOException e )
+        {
+            // then
+            assertThat( e, instanceOf( FileNotFoundException.class ) );
         }
     }
 }
