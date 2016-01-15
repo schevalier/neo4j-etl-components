62f77b345a30c539b0a3ac6a9acc33e38ffce61b
iansrobinson <iansrobinson@gmail.com>
2016-01-15 10:35:35 +0000

    Adds test that ensures MySqlExportFileWriter creates export file path.

--- mysql-connector/src/main/java/org/neo4j/integration/io/AwaitHandle.java
+++ mysql-connector/src/main/java/org/neo4j/integration/io/AwaitHandle.java
@@ -1,10 +1,35 @@
 package org.neo4j.integration.io;
 
+import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public interface AwaitHandle<T>
 {
+    static <T> AwaitHandle<T> noOp()
+    {
+        return new AwaitHandle<T>()
+        {
+            @Override
+            public T await() throws Exception
+            {
+                return null;
+            }
+
+            @Override
+            public T await( long timeout, TimeUnit unit ) throws Exception
+            {
+                return null;
+            }
+
+            @Override
+            public CompletableFuture<T> toFuture()
+            {
+                return CompletableFuture.completedFuture( null );
+            }
+        };
+    }
+
     T await() throws Exception;
     T await(long timeout, TimeUnit unit) throws Exception;
     CompletableFuture<T> toFuture();
---
+++ mysql-connector/src/test/java/org/neo4j/integration/sql/exportcsv/mysql/MySqlExportFileWriterTest.java
@@ -0,0 +1,53 @@
+package org.neo4j.integration.sql.exportcsv.mysql;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import org.neo4j.integration.io.AwaitHandle;
+import org.neo4j.integration.neo4j.importcsv.config.Formatting;
+import org.neo4j.integration.sql.SqlRunner;
+import org.neo4j.integration.sql.exportcsv.ExportSqlSupplier;
+import org.neo4j.integration.sql.exportcsv.config.ExportToCsvConfig;
+import org.neo4j.integration.sql.exportcsv.mapping.ColumnToCsvFieldMappings;
+import org.neo4j.integration.sql.metadata.ConnectionConfig;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MySqlExportFileWriterTest
+{
+    @Test
+    public void shouldCreateExportFilePathAndPassItToSqlSupplier() throws Exception
+    {
+        // given
+        String sql = "SELECT ...";
+        Path expectedPath = Paths.get( "/test/movies.csv" );
+
+        SqlRunner sqlRunner = mock( SqlRunner.class );
+        when( sqlRunner.execute( sql ) ).thenReturn( AwaitHandle.noOp() );
+
+        ColumnToCsvFieldMappings mappings = mock( ColumnToCsvFieldMappings.class );
+
+        ExportSqlSupplier sqlSupplier = mock( ExportSqlSupplier.class );
+        when( sqlSupplier.sql( mappings, expectedPath ) ).thenReturn( sql );
+
+        ExportToCsvConfig config = ExportToCsvConfig.builder()
+                .destination( Paths.get( "/test" ) )
+                .connectionConfig( mock( ConnectionConfig.class ) )
+                .formatting( Formatting.DEFAULT )
+                .build();
+
+        MySqlExportFileWriter writer = new MySqlExportFileWriter( config, sqlRunner );
+
+        // when
+        Path exportFile = writer.writeExportFile( mappings, sqlSupplier, "movies" );
+
+        // then
+        verify( sqlSupplier ).sql( mappings, expectedPath );
+        assertEquals( expectedPath, exportFile );
+    }
+}
